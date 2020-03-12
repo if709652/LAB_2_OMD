83aa932a7f6588248666368c20c4cfa091f98b53
if709652 <31251179+if709652@users.noreply.github.com>
2020-03-12 08:38:29 -0600

    Add files via upload

---
+++ principal.py
@@ -0,0 +1,27 @@
+import funciones as fn
+import pandas as pd
+
+#%%
+datos = fn.f_leer_archivo('archivo_tradeview_1.xlsx')
+
+import funciones as fn
+
+# no esta completo
+
+
+def importar_precios(n_archivo):
+    data = pd.read_csv(n_archivo)
+    return data
+
+
+importar_precios('trade3.XLS')
+
+def f_pip_size(param_inst):
+
+
+
+    #inst=inst.lower()
+    #pips_inst={'usdjpy':100,
+       #     'gbpjpy':100}
+
+    return  1
